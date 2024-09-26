88702823a12742a06202ce589fbce76496a86856
olki123 <142659418+olki123@users.noreply.github.com>
2024-09-26 10:01:58 +0100

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+console.log(
+  (document.querySelector('.message').textContent = 'correct number')
+);
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+
+console.log(document.queryselector('.guess').value);
+*/
+
+// 1. Here add event is click only ,function is a value and function can be convert into other function.
+
+//2. our event name is click and the other argument is function
+
+const number = Math.trunc(Math.random() * 20) + 1
+
+const x = function({
+  console.log(23)
+})
+
+document.querySelector('body').style.backgroundColor = '#60b347' // the value  always be on string after equal
+
+// use camel case in java script when two words
+
+ducument,querySelector('.number').style.width = '30rem' // always in string
+
+document.querySelector('.check').addEventListener('click' function(){
+  const guess = document.querySelector('guess').value
+  console.log(guess, typeof guess)
+  
+  if (!guess) {  // guess is zero and zero is falsy value . it will be converted to false.
+
+  }
+});
+
+// DRY principle mean Don't repeat yourself  in code
+
+// Use refactory method: When more repetitive code , you can refactor (less) it by creating function and called the function. 
+ 
